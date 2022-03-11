d3aeb8f999ca30917688deb160c490356a0fb4b6
Alexandre Guillome <alexandre1906@live.fr>
2022-03-11 02:08:07 +0100

    Profil + CSS navbar + modif backend + upload image

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.scss';
 
+//Redux
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'
+
+//Dev Tools
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
+
+// Ne pas oublier de virer le console TOOLs pour le REDUX
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
+)
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
